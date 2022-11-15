2e4958c2c3c12e2a6958fcad2c2bab642a88811d
Jakub <jakubnagadowski123@gmail.com>
2022-11-15 18:04:57 +0100

    created function for initializing first population

--- py/GA-TSM-main.py
+++ py/GA-TSM-main.py
@@ -8,3 +8,12 @@ from Fitness import Fitness
 def route(cityList):
     route = random.sample(cityList, len(cityList))
     return route
+
+# creating initial population, first generation
+
+
+def initialPop(popSize, cityList):
+    population = []
+    for i in range(0, popSize):
+        population.append(route(cityList))
+    return population
